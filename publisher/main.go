faef8fcd671126bb3d9adf1b4f3ee1cf069fec44
Eduardo Barbosa <eduardocrp@gmail.com>
2024-05-31 11:27:30 +0000

    add message args

--- publisher/main.go
+++ publisher/main.go
@@ -10,17 +10,27 @@ import (
 var publisher = kafka.DefaultKafkaPublisher{}
 
 func main() {
-	appName := flag.String("app_name", "origem_app", "Aplicação de origem")
-	criticalLevel := flag.String("critical_level", "ERROR", "Nível de exceção")
+	message := loadMessageArgs()
+
+	if err := message.Publish(publisher); err != nil {
+		log.Fatalf("Falha ao publicar: %s", err.Error())
+	}
+}
+
+func loadMessageArgs() pub.MessageException {
+	appName := flag.String("app_name", "", "Aplicação de origem")
+	criticalLevel := flag.String("critical_level", "", "Nível de exceção")
+	id := flag.String("id", "", "Identificador")
+	flow := flag.String("flow", "", "Fluxo a ser mapeado")
+	msg := flag.String("msg", "", "Messagem de exceção")
 	flag.Parse()
 
 	var message = pub.MessageException{
 		ApplicationName: *appName,
 		CriticalLevel:   *criticalLevel,
+		ID:              *id,
+		Flow:            *flow,
+		Message:         *msg,
 	}
-
-	message.Flow = "flow-test"
-	if err := message.Publish(publisher); err != nil {
-		log.Fatalf("Falha ao publicar: %s", err.Error())
-	}
+	return message
 }
